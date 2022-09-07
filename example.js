fceefc92905d0732bf811f60ebb6960928493ec9
unknown <geunwon1947@gmail.com>
2022-09-07 12:54:54 +0900

    삼항연산자

---
+++ example.js
@@ -0,0 +1,14 @@
+let person = {
+  name:'tony',
+  age:20,
+  driver:null
+};
+
+// if(person.age >= 16){
+//   person.driver = 'yes';
+// }else{
+//   person.driver = 'no';
+// }
+
+let result = person.age>=16 ? "yes" : "no";
+console.log(result);
