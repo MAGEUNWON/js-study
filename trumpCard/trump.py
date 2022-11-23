32e3dd66831c52449368b85e1120b4116ffcc843
unknown <geunwon1947@gmail.com>
2022-11-23 18:03:32 +0900

    덱 자료구조 만들기

---
+++ trumpCard/trump.py
@@ -0,0 +1,19 @@
+shape_array = ["spade", "heart", "diamond", "club", "joker"]
+
+datas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+dic  = dict()
+
+def trump_deck():
+    for i in shape_array:
+        dic = {i}
+        # dict.keys(i)
+        print(dic)
+
+# 객체 안에 shape_array 배열 원소가 각각 들어가긴했는데 키와 값으로 어떻게 넣어야 할 지 모르겠음
+
+
+    
+
+trump_deck()
+
