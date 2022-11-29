f8cc86d35c6a0205b50944c756eee9fe77d8c9d6
unknown <geunwon1947@gmail.com>
2022-11-29 12:15:03 +0900

    switch, case문 함수로 바꿔보기(모든 리터럴을 없애볼 것), git commit 자주 하기, text/json 형태가 api 받아오는 형식. 다른 형식도 알아 보기

--- server/app.js
+++ server/app.js
@@ -4,14 +4,59 @@ var fs = require("fs");
 // import * as fs from 'fs'; 앞에 * 붙여주면 fs의 모든 것을 다 쓰겠다는 뜻. 이렇게 써줘야 하나씩 설정 안하고 다 쓸 수 있음.
 var http = require("http");
 var server = http.createServer(function (req, res) {
+    var _url = req.url;
+    var readfiles = function (path) {
+        return fs.readFileSync("".concat(path, ".txt"), 'utf8');
+    };
+    var data = "\n  ".concat(readfiles('header'), "\n  ").concat(readfiles('main'), "\n  ").concat(readfiles('footer'), "\n  ");
+    var input = "\n    ".concat(readfiles('head'), "\n  ");
     if (req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        var readfiles = function (path) {
-            return fs.readFileSync("".concat(path, ".txt"), 'utf8');
-        };
-        var data = "\n      ".concat(readfiles('header'), "\n      ").concat(readfiles('main'), "\n      ").concat(readfiles('footer'), "\n    ");
-        fs.writeFileSync('body.txt', data, 'utf8');
-        res.end("\n      ".concat(readfiles('head'), "\n      ").concat(readfiles('body'), "\n    "));
+        // switch (_url === "/") {
+        //   case _url === "/":
+        //     console.log("/라우터")
+        //     res.writeHead(200,{'Content-Type':'text/html'})
+        //     fs.writeFileSync('body.txt',data,'utf8')
+        //     res.end(`
+        //       ${readfiles('head')}
+        //       ${readfiles('body')}
+        //     `)
+        //     break;
+        //   case _url === "/a":
+        //     console.log("a 라우터")
+        //     // res.writeHead(200,{'Content-Type':'text/html'})
+        //     // res.write()
+        //     // res.end()
+        // }
+        if (_url === "/") {
+            console.log("/라우터");
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            fs.writeFileSync('body.txt', data, 'utf8');
+            res.end("\n        ".concat(readfiles('head'), "\n        ").concat(readfiles('body'), "\n      "));
+        }
+        else if (_url === "/a") {
+            console.log("a라우터");
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            fs.writeFileSync('body.txt', "a라우터입니다", 'utf8');
+            res.end("\n      ".concat(readfiles('head'), "\n      ").concat(readfiles('body'), "\n      "));
+        }
+        else if (_url === "/b") {
+            console.log("b라우터");
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            fs.writeFileSync('body.txt', "b라우터입니다", 'utf8');
+            res.end("\n      ".concat(readfiles('head'), "\n      ").concat(readfiles('body'), "\n      "));
+        }
+        else if (_url === "/c") {
+            console.log("c라우터");
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            fs.writeFileSync('body.txt', "c라우터입니다", 'utf8');
+            res.end("\n      ".concat(readfiles('head'), "\n      ").concat(readfiles('body'), "\n      "));
+        }
+        else if (_url === "/d") {
+            console.log("d라우터");
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            fs.writeFileSync('body.txt', "d라우터입니다", 'utf8');
+            res.end("\n      ".concat(readfiles('head'), "\n      ").concat(readfiles('body'), "\n      "));
+        }
     }
 });
 server.listen(5000, function () {
--- server/app.ts
+++ server/app.ts
@@ -1,30 +1,132 @@
 import * as fs from 'fs';
 // import * as fs from 'fs'; 앞에 * 붙여주면 fs의 모든 것을 다 쓰겠다는 뜻. 이렇게 써줘야 하나씩 설정 안하고 다 쓸 수 있음.
 import * as http from 'http';
+// const url = require('url');
 
 interface readfiles{
   (path:string):void
 }
 
 const server = http.createServer((req:http.IncomingMessage , res:http.ServerResponse) =>{
-  if(req.method === 'GET'){
-    res.writeHead(200,{'Content-Type':'text/html'})
-    const readfiles:readfiles =(path)=>{
-      return fs.readFileSync(`${path}.txt`,'utf8')
+
+  const _url = req.url;
+
+  const readfiles:readfiles =(path)=>{
+    return fs.readFileSync(`${path}.txt`,'utf8')
     } 
+
+  let data = `
+  ${readfiles('header')}
+  ${readfiles('main')}
+  ${readfiles('footer')}
+  `
+
+
   
-    let data = `
-      ${readfiles('header')}
-      ${readfiles('main')}
-      ${readfiles('footer')}
-    `
-    fs.writeFileSync('body.txt',data,'utf8')
+  if(req.method === 'GET'){
+
+    switch (_url) {
+      case "/":
+
+        console.log("/라우터")
+        res.writeHead(200,{'Content-Type':'text/html'})
+        fs.writeFileSync('body.txt',data,'utf8')    
+        res.end(`
+          ${readfiles('head')}
+          ${readfiles('body')}
+        `)
+        break;
+      
+      case "/a":
+        console.log("a 라우터")
+        res.writeHead(200,{'Content-Type':'text/html'})
+        fs.writeFileSync('body.txt',"a라우터입니다",'utf8')
+        res.end(`
+        ${readfiles('head')}
+        ${readfiles('body')}
+        `)
+        break;
+
+      case "/b":
+        console.log("b라우터")
+        res.writeHead(200,{'Content-Type':'text/html'})
+        fs.writeFileSync('body.txt',"b라우터입니다",'utf8')
+        res.end(`
+        ${readfiles('head')}
+        ${readfiles('body')}
+        `)
+        break;
+
+      case "/c":
+        console.log("c 라우터")
+        res.writeHead(200,{'Content-Type':'text/html'})
+        fs.writeFileSync('body.txt',"c라우터입니다",'utf8')
+        res.end(`
+        ${readfiles('head')}
+        ${readfiles('body')}
+        `)
+        break;
+
+      case "/d":
+        console.log("d 라우터")
+        res.writeHead(200,{'Content-Type':'text/html'})
+        fs.writeFileSync('body.txt',"d라우터입니다",'utf8')
+        res.end(`
+        ${readfiles('head')}
+        ${readfiles('body')}
+        `)
+        break;
+    }
+    // *switch case 문 함수로 바꿔보기(모든 리터럴을 없애볼 것) , git commit 하기, text/json일 경우가 api 받아오는 형식 . 
+
+    // if (_url === "/") {
+
+    //   console.log("/라우터")
+    //   res.writeHead(200,{'Content-Type':'text/html'})
+    //   fs.writeFileSync('body.txt',data,'utf8')
   
-    res.end(`
-      ${readfiles('head')}
-      ${readfiles('body')}
-    `)
-  }
+    //   res.end(`
+    //     ${readfiles('head')}
+    //     ${readfiles('body')}
+    //   `)
+        
+    // } else if(_url === "/a") {
+    //   console.log("a라우터")
+    //   res.writeHead(200,{'Content-Type':'text/html'})
+    //   fs.writeFileSync('body.txt',"a라우터입니다",'utf8')
+    //   res.end(`
+    //   ${readfiles('head')}
+    //   ${readfiles('body')}
+    //   `)
+
+    // } else if(_url === "/b") {
+    //   console.log("b라우터")
+    //   res.writeHead(200,{'Content-Type':'text/html'})
+    //   fs.writeFileSync('body.txt',"b라우터입니다",'utf8')
+    //   res.end(`
+    //   ${readfiles('head')}
+    //   ${readfiles('body')}
+    //   `)
+
+    // } else if(_url === "/c") {
+    //   console.log("c라우터")
+    //   res.writeHead(200,{'Content-Type':'text/html'})
+    //   fs.writeFileSync('body.txt',"c라우터입니다",'utf8')
+    //   res.end(`
+    //   ${readfiles('head')}
+    //   ${readfiles('body')}
+    //   `)
+      
+    // } else if(_url === "/d") {
+    //   console.log("d라우터")
+    //   res.writeHead(200,{'Content-Type':'text/html'})
+    //   fs.writeFileSync('body.txt',"d라우터입니다",'utf8')
+    //   res.end(`
+    //   ${readfiles('head')}
+    //   ${readfiles('body')}
+    //   `)
+    // }
+  } 
 })
 
 
