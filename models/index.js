b4278653a0f03a357d3ab658ec4d28c94cd618a6
unknown <geunwon1947@gmail.com>
2022-12-07 09:50:04 +0900

    MYSQL 연습

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const User = require('./user');
 const Comment = require('./comment');
 
-const env = process.env.NODE_ENV || 'production'; // 개발할 땐 develpment 배포 할 땐 production
+const env = process.env.NODE_ENV || 'development'; // 개발할 땐 develpment 배포 할 땐 production
 const config = require('../config/config')[env];
 const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
